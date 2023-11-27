178fa5da7f383d9eede0a88e8e2f0a51458f0ebd
kbosmi005 <dasia007d-mirinsg3@yahoo.co.jp>
2023-11-27 22:41:55 +0900

    サービスへの切り離し

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
 use App\Http\Requests\UploadImageRequest;
+use App\Services\ImageService;
 
 
 class ShopController extends Controller
@@ -52,18 +53,9 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
-        error_log("デバッグ情報：このメッセージがログに記録されます。");
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid() ){
-            // Storage::putFile('public/shops',$imageFile);
-            $fileName = uniqid(rand().'_');
-            $extension = $imageFile->extension();
-            $fileNameToStore = $fileName.'.'.$extension;
-            // $resizedImage = InterventionImage::make($imageFile)->resize(1920,1080)->encode();
-            $resizedImage = InterventionImage::make($imageFile->path())->resize(1920, 1080)->encode();
-            // dd($imageFile,$resizedImage);
-
-            Storage::put('public/shops/'.$fileNameToStore,$resizedImage);
+            $fileNameToStore = ImageService::upload($imageFile,'shops');
         }
 
         return redirect()
