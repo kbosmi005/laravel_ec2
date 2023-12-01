6d89a112e531143ffb219734fb59a9d23a0dca1f
kbosmi005 <dasia007d-mirinsg3@yahoo.co.jp>
2023-12-01 17:12:11 +0900

    category

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-git clone
-
-git clone https://github.com/aokitashipro/laravel_umarche.git
-
-git clone ブランチを指定してダウンロードする場合
-
-git clone -b ブランチ名 https://github.com/aokitashipro/laravel_umarche.git
-
-もしくはzipファイルでダウンロードしてください
 
 ○ インストール方法
 
@@ -41,13 +32,3 @@ git clone -b ブランチ名 https://github.com/aokitashipro/laravel_umarche.git
     storage/app/public/productsフォルダ内に 保存すると表示されます。
     (productsフォルダがない場合は作成してください。)
     ショップの画像も表示する場合は、 storage/app/public/shopsフォルダを作成し 画像を保存してください。
-
-○ section7の補足
-
-    決済のテストとしてstripeを利用しています。 必要な場合は .env にstripeの情報を追記してください。 (講座内で解説しています)
-
-○ section8の補足
-
-    メールのテストとしてmailtrapを利用しています。 必要な場合は .env にmailtrapの情報を追記してください。 (講座内で解説しています)
-    メール処理には時間がかかるので、 キューを使用しています。
-    必要な場合は php artisan queue:workで ワーカーを立ち上げて動作確認するようにしてください。 (講座内で解説しています)
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('primary_categories')->insert([
+            [
+                'name' => '漬物',
+                'sort_order' => 1,
+            ],
+            [
+                'name' => '魚介類・水産加工品',
+                'sort_order' => 2,
+            ],
+            [
+                'name' => 'チーズ・乳製品',
+                'sort_order' => 3,
+            ],
+        ]);
+
+        DB::table('secondary_categories')->insert([
+            [
+                'name' => '梅干し',
+                'sort_order' => 1,
+                'primary_category_id' => 1
+            ],
+            [
+                'name' => 'キムチ',
+                'sort_order' => 2,
+                'primary_category_id' => 1
+
+            ],
+            [
+                'name' => 'メンマ',
+                'sort_order' => 3,
+                'primary_category_id' => 1
+
+            ],
+            [
+                'name' => '魚卵',
+                'sort_order' => 4,
+                'primary_category_id' => 2
+
+            ],
+            [
+                'name' => 'カニ',
+                'sort_order' => 5,
+                'primary_category_id' => 2
+
+            ],
+            [
+                'name' => '加工品',
+                'sort_order' => 6,
+                'primary_category_id' => 2
+
+            ],
+        ]);
+
+
+
+    }
+}
