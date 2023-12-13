c5a25725794483a2c2c23692b453da7c9617ec2b
kbosmi005 <dasia007d-mirinsg3@yahoo.co.jp>
2023-12-13 15:23:28 +0900

    一旦終了

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -4,35 +4,33 @@ namespace App\Services;
 use App\Models\Product;
 use App\Models\Cart;
 
-
 class CartService
 {
-
     public static function getItemsInCart($items)
     {
         $products = [];
 
-
-        foreach ($items as $item)
+        foreach($items as $item)
         {
             $p = Product::findOrFail($item->product_id);
-            $owner = $p->shop->owner->select('name','email')->first()->toArray();
-            $values = array_values($owner);
-            $keys = ['ownerName','email'];
-            $ownerInfo = array_combine($keys,$values);
+            $owner = $p->shop->owner;
+            $ownerInfo = [
+                'ownerName' => $owner->name,
+                'email' => $owner->email
+            ];
 
-            $product = Product::where('id',$item->product_id)
-            ->select('id','name','price')->get()->toArray();
+            $product = Product::where('id', $item->product_id)
+            ->select('id', 'name', 'price')->get()->toArray();
 
-            $quantity = Cart::where('product_id',$item->product_id)
+            $quantity = Cart::where('product_id', $item->product_id)
             ->select('quantity')->get()->toArray();
 
-            $result = array_merge($product[0],$ownerInfo,$quantity[0]);
+            $result = array_merge($product[0], $ownerInfo, $quantity[0]);
 
-            array_push($products,$result);
-        }
-        dd($products);
-        return $products;
+            array_push($products, $result);
     }
 
+    return $products;
+
+  }
 }
